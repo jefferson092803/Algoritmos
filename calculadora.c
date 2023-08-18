ef8ed40b6ec65e54da1bb4861c86465f3d1260af
jefferson092803 <jeffersonlucas810@gmail.com>
2023-08-18 11:07:06 -0300

    calcuradoraa

---
+++ calculadora.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int numero1;    
+    int numero2;
+    int resultado;
+   
+    numero1 =20;
+    numero2 = 3;
+
+    resultado = numero1 % numero2;
+    printf("Resultado:%d\n",resultado);
+
+    return 0; 
+}
