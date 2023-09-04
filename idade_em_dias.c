65c40043460899f4cf63393fbe00a72c839a6feb
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-09-04 15:34:52 -0300

    Aprimorei o exercicio de idade em dias

--- idade_em_dias.c
+++ idade_em_dias.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
  
 int main() {
-    int dias,meses,anos,diasRest;
+    int dias,meses,anos,D;
     scanf("%d",&dias);
     anos=dias / 365;
-    diasRest=dias%365;
     meses=(dias%365)/30;
-    dias=diasRest%30;
-    printf("%d\n ano(s)",anos);
-    printf("%d\n mes(es)",meses);
-    printf("%d\n dias(s)",diasRest);
+    D=(dias%365)%30;
+    printf("%d ano(s)\n %d mes(es)\n %d dia(s)",anos,meses,D);
     return 0;
 }
