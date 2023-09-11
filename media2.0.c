c6592aca5f66092e413bb0e30812bcf6cb0626af
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-09-11 19:11:31 -0300

    Criei o segundo exx de media

---
+++ media2.0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    double A;
+    double B;
+    double C;
+    scanf("%lf",&A);
+    scanf("%lf",&B);
+    scanf("%lf",&C);
+    printf("MEDIA = %.1lf\n",(A*2+B*3+C*5)/10);
+    
+
+
+    return 0;
+}
