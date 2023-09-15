74eacef72b9f74c554bb2b80e32ee96f2806fbdb
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-09-15 15:57:46 -0300

    Exercicio da lista feito

--- Lista_Avaliativa/exercicio2.c
+++ Lista_Avaliativa/exercicio2.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(){
-   int numero;
+   int soma= 0;
+   int numeroOriginal,numero;
    printf("Digite um numero inteiro positivo: \n");
    scanf("%d",&numero);
    if (numero % 2 == 0)
@@ -14,7 +15,13 @@ int main(){
     printf("%d",numero);
     printf(" eh impar\n");
    }
-
-
+   numeroOriginal = numero;
+   while (numero != 0)
+   {
+    soma += numero % 10;
+    numero /= 10;
+   }
+   
+   printf("A soma dos algarismos de %d eh %d.\n",numeroOriginal,soma);
     return 0;
 }
