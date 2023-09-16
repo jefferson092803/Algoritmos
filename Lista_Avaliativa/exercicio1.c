f2dacc73480d9cda6fcc903010e532ab653ce078
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-09-16 19:36:22 -0300

    .

--- Lista_Avaliativa/exercicio1.c
+++ Lista_Avaliativa/exercicio1.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 int main(){
-   int X;
-   scanf("%d",&X);
-   while (X < 1800 || X >2022)
+   int ano;
+   scanf("%d",&ano);
+   
+   
+   if ((ano - 1896) % 4 ==0 )
    {
-     printf("Entrada inválida.Por favor insira um valor entre 1800 e 2022.\n");
+     printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d\n",ano);
+   }else
+   {
+     printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d\n",ano);
    }
-   
-   
+   if (ano >= 1930 && (ano -1930)% 2 == 0)
+   {
+    printf("A Copa do Mundo de Futebol ocorreu no ano de %d\n",ano);
+   }else
+   {
+    printf(" “Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d\n",ano);
+   }
    
    
     return 0;
---
+++ Lista_Avaliativa/exercicio4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+   float salario;
+   char nivelFuncionario;
+   scanf("%c %f",&nivelFuncionario,&salario);
+   if (nivelFuncionario = "a")
+   {
+     printf("R \\ $ ");
+   }
+   
+
+
+
+    return 0;
+}
