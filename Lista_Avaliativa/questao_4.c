39474503748e20027e8eb3e9db3bcb2438c18624
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-09-16 21:15:33 -0300

    atualizado

---
+++ Lista_Avaliativa/questao_4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int main(){
+   float salario;
+   char nivelFuncionario;
+   float porcentagem;
+
+   printf("Digite o nivel do funcionario:\n");
+   scanf("%c",&nivelFuncionario);
+   printf("Digite o salario do funcionario\n");
+   scanf("%f",&salario);
+   switch (nivelFuncionario)
+   {
+   case 'a':
+      porcentagem =+ salario * 0.05;
+      printf("R$ %.2f",salario + porcentagem);
+    break;
+   case 'b':
+      porcentagem=+ salario *0.05;
+      printf("R$ %.2f",salario + porcentagem);
+   case 'c':
+      porcentagem =+ salario * 0.08;
+      printf("R$ %.2f",salario + porcentagem);
+    break;
+   default:
+      printf("Nivel inexistente");
+    break;
+   }
+   
+    return 0;
+}
