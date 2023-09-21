d58b8e710e13ade040da0ba65115077d0a098b1f
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-09-20 21:48:38 -0300

    .

--- Lista_Avaliativa/questao_1.c
+++ Lista_Avaliativa/questao_1.c
@@ -3,20 +3,20 @@
 
 int main(){
     
-    int ano;
+    int ano;    
     scanf("%d",&ano);
-    if (ano%4==0 && ano!=1916||1940||1944)
+     if((ano >= 1896 && ano <= 2020) && (ano % 4 == 0))
     {
         printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d\n",ano);
-    }else if (ano%4==2 && ano!=1916||1940||1944)
+    } 
+    else if (ano >= 1930 && (ano % 4 == 2))
     {
         printf("A Copa do Mundo de Futebol ocorreu no ano de %d\n",ano);
-    }else
+    }
+    else
     {
         printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d\n",ano);
     }
-    
-    
     
     return 0;
 }
