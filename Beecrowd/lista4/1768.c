d975b79bb0d4933efcce4d8baf73f71c3be33712
jefferson092803 <jeffersonlucas810@gmail.com>
2023-09-29 11:35:54 -0300

    ..

--- Beecrowd/lista4/1768.c
+++ Beecrowd/lista4/1768.c
@@ -3,8 +3,29 @@
 int main(){
     
     int tamanhoArvore;
-    scanf("%d",tamanhoArvore);
-    
+    scanf("%d",&tamanhoArvore);
+    for (int  i = 0; i < tamanhoArvore; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    int meioArvore= tamanhoArvore/2;
+    //linha de cima da base("tronco da árvore")
 
+    for (int i = 0; i < meioArvore; i++)
+    {
+        printf(" ");
+    }
+    
+    printf("*\n");
+
+    //linha de baixo da base
+    
+    for (int i = 0; i < meioArvore - 1; i++)
+    {
+        printf(" ");
+    }
+    printf("***\n");
+    
     return 0;
 }
