f03eaa7a448865e9133a4e48d3ed89a35e1070e0
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-10-01 07:32:47 -0300

    ..

--- Beecrowd/lista4/1768.c
+++ Beecrowd/lista4/1768.c
@@ -1,31 +1,41 @@
 #include <stdio.h>
 
 int main(){
-    
+
     int tamanhoArvore;
-    scanf("%d",&tamanhoArvore);
-    for (int  i = 0; i < tamanhoArvore; i++)
-    {
-        printf("*");
-    }
-    printf("\n");
-    int meioArvore= tamanhoArvore/2;
-    //linha de cima da base("tronco da árvore")
 
-    for (int i = 0; i < meioArvore; i++)
-    {
-        printf(" ");
-    }
-    
-    printf("*\n");
+    while(scanf("%d", &tamanhoArvore) == 1){
+        int meioArvore = tamanhoArvore/2;
+        int numLinhas = meioArvore + 1;
+        int numAstericos = 1;
+        int numEspacos = meioArvore;
 
-    //linha de baixo da base
-    
-    for (int i = 0; i < meioArvore - 1; i++)
-    {
-        printf(" ");
+        // percorre linha por linha da parte com "folhas" da árvore
+        for(int linhaArvore = 0;linhaArvore < numLinhas;linhaArvore++){
+            // imprime espaços de cada linha
+            for(int espacosImpressos = 0;espacosImpressos < numEspacos;espacosImpressos++){
+                printf(" ");
+            }
+            // imprime os asteriscos de cada linha
+            for(int asteriscosImpressos = 0;asteriscosImpressos < numAstericos;asteriscosImpressos++){
+                printf("*");
+            }
+            printf("\n");
+            numAstericos = numAstericos + 2;
+            numEspacos--;
+        }
+
+        // linha de cima da base ("tronco da árvore")
+        for(int i = 0;i < meioArvore;i++){
+            printf(" ");
+        }
+        printf("*\n");
+
+        // linha de baixo da base
+        for(int i = 0;i < meioArvore - 1;i++){
+            printf(" ");
+        }
+        printf("***\n\n");
     }
-    printf("***\n");
-    
     return 0;
 }
