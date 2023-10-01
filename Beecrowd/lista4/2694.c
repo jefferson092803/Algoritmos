df9491329e3c8c8d43dedb931a34b6ba9d84c417
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-10-01 07:51:51 -0300

    ..

---
+++ Beecrowd/lista4/2694.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+   int quantidade_Entrada;
+   scanf("%d",&quantidade_Entrada);
+   for (int i = 0; i < quantidade_Entrada; i++)
+   {
+      char string[14];
+      scanf("%s",string);
+      char num1[2],num2[3];
+   }
+   
+
+
+
+    return 0;
+}
