41ff28472ab513edc352d54afb09be338c06c27a
jefferson092803 <jeffersonlucas810@gmail.com>
2023-10-10 09:49:06 -0300

    ..

--- Lista_Avaliativa/questao_3.c
+++ Lista_Avaliativa/questao_3.c
@@ -4,8 +4,8 @@ int main(){
     int senhaNova,senhaCadastrada;
 
     scanf("%d", &senhaCadastrada);
-    printf("senha cadastrada:%.4d\n",senhaCadastrada);
-    
+    printf("senha cadastrada: %.4d\n",senhaCadastrada);
+
     while(1){
         scanf("%d", &senhaNova);
         if(senhaNova == senhaCadastrada){
