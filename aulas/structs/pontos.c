782f03a7cad8ad23d524ca28fbc4a9ec690c4b7c
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-10-17 14:30:14 -0300

    aprendi classe em C(struct)

--- aulas/structs/pontos.c
+++ aulas/structs/pontos.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
+#include <math.h>
 
-
-struct ponto
-{
-    int X;
-    int Y;
+struct ponto{
+    int x;
+    int y;
 };
 
 
-
-
 int main(){
-    struct ponto p1;
+    struct ponto p1, p2;
 
-    scanf("%d",&p1.X);
-    scanf("%d",&p1.Y);
+    printf("Insira as coordenadas do ponto p1:\n");
+    printf("x: ");
+    scanf("%d", &p1.x);
+    printf("y: ");
+    scanf("%d", &p1.y);
+
+    printf("Insira as coordenadas do ponto p2:\n");
+    printf("x: ");
+    scanf("%d", &p2.x);
+    printf("y: ");
+    scanf("%d", &p2.y);
 
 
-    printf("Distancia entre p1(%d ,%d) e (0,0):\n",p1.X,p1.Y);
-    printf("%f",sqrt(p1.X*p1.X + p1.Y * p1.Y));
-
-
-
-
-
+    printf("distancia entre p1 (%d, %d) e (0, 0):\n", p1.x, p1.y);
+    printf("%f", sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y,2)));
 
     return 0;
 }
