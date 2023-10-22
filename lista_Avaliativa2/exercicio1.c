a643a9d86b0e09f6ba3b19dc5697856da1120878
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-10-22 08:07:10 -0300

    lista 6

--- lista_Avaliativa2/exercicio1.c
+++ lista_Avaliativa2/exercicio1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(){
-
+    int num = 10;
     int valores[10];
-     for (int i = 0; i < 10; i++) {
+     for (int i = 0; i < num; i++) {
         scanf("%d", &valores[i]);
      }
-      for (int i = 0; i < 10; i++) {
+      for (int i = 0; i < num; i++) {
         printf("%d ", valores[i]);
     }
     printf("\n");
