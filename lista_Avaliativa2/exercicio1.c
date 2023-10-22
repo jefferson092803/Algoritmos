c393ba934eb2bc69932c76b6e3fc6925c3f65273
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-10-22 08:26:35 -0300

    questão 1 finalizada

--- lista_Avaliativa2/exercicio1.c
+++ lista_Avaliativa2/exercicio1.c
@@ -3,15 +3,33 @@
 int main(){
     int num = 10;
     int valores[10];
-     for (int i = 0; i < num; i++) {
+    int conjunto_novo[10];
+    for (int i = 0; i < num; i++) {
         scanf("%d", &valores[i]);
-     }
-      for (int i = 0; i < num; i++) {
-        printf("%d ", valores[i]);
     }
-    printf("\n");
-
-
+    while (num > 1)
+    {
+         for (int i = 0; i < num; i++) {
+            printf("%d", valores[i]);
+            
+            if (i < num - 1) {
+                printf(" ");
+             }
+         }
+         printf("\n");
+         for (int i = 0; i < num - 1; i++)
+         {
+            conjunto_novo[i] = valores[i] + valores[i+1];
+         }
+         num--;
+         for (int i = 0; i < num; i++)
+         {
+            valores[i] = conjunto_novo[i];
+         }
+         
+    }
+    printf("%d\n", valores[0]);
+    
 
 
     return 0;
