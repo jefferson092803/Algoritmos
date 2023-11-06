63b6ac10ae28c17c6e449e4d82d3c9a8f1fbaad1
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-11-05 21:23:01 -0300

    ..

--- lista_Avaliativa2/exercicio2.c
+++ lista_Avaliativa2/exercicio2.c
@@ -1,40 +1,57 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+float calculaForcaPonderada(int forcas[]) {
+  const int weights[] = {8, 10, 10, 5, 5, 8, 8, 11, 11, 12, 12};
+  return (weights[0]*forcas[0]+weights[1]*(forcas[1] + forcas[2])+weights[2]*(forcas[3]+forcas[4])+weights[3]*(forcas[5]+forcas[6])+weights[4]*(forcas[7]+forcas[8])+weights[5]*(forcas[9]+forcas[10]))/ 100.0;
+}
+
 
 int main(){
     char nomeDoTimeA[31];
     char nomeDoTimeB[31];
     char nomeJogador[31];
     char posicao;
-    int forca, G1 = 0, G2 = 0, L1 = 0, L2 = 0, Z1 = 0, Z2 = 0, V1 = 0, V2 = 0, M1 = 0, M2 = 0, A1 = 0, A2 = 0;
-    
-    fgets(nomeDoTimeA,31,stdin);
+    int forcasTimeA[11],forcasTimeB[11];
+    int forca;
+      
+    scanf(" %30[^\n]", nomeDoTimeA);
     for (int i = 0; i < 11; i++)
     {
-        fgets(nomeJogador,31,stdin);
-         char * token = strtok(nomeJogador,";");
-         token = strtok(NULL, ";");
-         token = strtok(NULL, ";");
-         int valor = atoi(token);
-        if (strcmp(posicao,"G")==0) {
-            G1 = forca;
-        } else if (strcmp(posicao,"L")== 0) {
-            L1 += forca;
-        } else if (strcmp(posicao,"Z")== 0) {
-            Z1 += forca;
-        } else if (strcmp(posicao,"V")== 0) {
-            V1 += forca;
-        } else if (strcmp(posicao,"M")== 0) {
-            M1 += forca;
-        } else if (strcmp(posicao,"A")== 0) {
-            A1 += forca;
-        }
+        scanf(" %30[^;]; %c; %d", nomeJogador,&posicao, &forca);
+        int posic = (posicao == 'L') ? (forcasTimeA[1] == 0) ? 1 : 2 : 
+        (posicao == 'Z') ? (forcasTimeA[3] == 0) ? 3 : 4 : 
+        (posicao == 'V') ? (forcasTimeA[5] == 0) ? 5 : 6 : 
+        (posicao == 'M') ? (forcasTimeA[7] == 0) ? 7 : 8 : 
+        (posicao == 'A') ? (forcasTimeA[9] == 0) ? 9 : 10 : 0;
+
+        if (forcasTimeA[posic] == 0)
+            forcasTimeA[posic] = forca;
     }
-    float forca_time1 = (8 * G1 + 10 * (L1 + L2) + 5 * (Z1 + Z2) + 8 * (V1 + V2) + 11 * (M1 + M2) + 12 * (A1 + A2)) / 100.0;
-    fgets(nomeDoTimeB,31,stdin);
     
-    printf("%s: %.2f de forca\n",nomeDoTimeA,forca_time1);
+    scanf(" %30[^\n]", nomeDoTimeB);
+    for (int i = 0; i < 11; i++) {
+        scanf(" %30[^;]; %c; %d", nomeJogador, &posicao, &forca);
+
+        int posi = (posicao == 'L') ? (forcasTimeB[1] == 0) ? 1 : 2 : 
+        (posicao == 'Z') ? (forcasTimeB[3] == 0) ? 3 : 4 : 
+        (posicao == 'V') ? (forcasTimeB[5] == 0) ? 5 : 6 : 
+        (posicao == 'M') ? (forcasTimeB[7] == 0) ? 7 : 8 : 
+        (posicao == 'A') ? (forcasTimeB[9] == 0) ? 9 : 10 : 0;
+
+        if (forcasTimeB[posi] == 0)
+            forcasTimeB[posi] = forca;
+    }
+    float forcaTimeA = calculaForcaPonderada(forcasTimeA);
+    float forcaTimeB = calculaForcaPonderada(forcasTimeB);
+    printf("%s: %.2f de forca\n%s: %.2f de forca\n", nomeDoTimeA, forcaTimeA, nomeDoTimeB, forcaTimeB);
+
+
+    if (forcaTimeA > forcaTimeB)
+        printf("%s eh mais forte\n", nomeDoTimeA);
+    else if (forcaTimeB > forcaTimeA)
+        printf("%s eh mais forte\n",nomeDoTimeB);
+    else
+        printf("Os times tem a mesma força\n");
+
     
 
 
