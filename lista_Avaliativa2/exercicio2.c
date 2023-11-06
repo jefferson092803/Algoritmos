4b65b42cafa018b9fd9d8bdc6b6ed04b649753af
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-11-05 21:41:46 -0300

    lista feita

--- lista_Avaliativa2/exercicio2.c
+++ lista_Avaliativa2/exercicio2.c
@@ -1,60 +1,54 @@
 #include <stdio.h>
+
 float calculaForcaPonderada(int forcas[]) {
-  const int weights[] = {8, 10, 10, 5, 5, 8, 8, 11, 11, 12, 12};
-  return (weights[0]*forcas[0]+weights[1]*(forcas[1] + forcas[2])+weights[2]*(forcas[3]+forcas[4])+weights[3]*(forcas[5]+forcas[6])+weights[4]*(forcas[7]+forcas[8])+weights[5]*(forcas[9]+forcas[10]))/ 100.0;
+    return (8 * forcas[0] + 10 * (forcas[1] + forcas[2]) + 5 * (forcas[3] + forcas[4]) + 8 * (forcas[5] + forcas[6]) + 11 * (forcas[7] + forcas[8]) + 12 * (forcas[9] + forcas[10])) / 100.0;
 }
 
+int main() {
+    int forcasTimeA[11] = {0}, forcasTimeB[11] = {0};
+    char nomeTimeA[31], nomeTimeB[31];
+    char nomeJogador[31], posicaoJogador;
+    int forcaJogador;
 
-int main(){
-    char nomeDoTimeA[31];
-    char nomeDoTimeB[31];
-    char nomeJogador[31];
-    char posicao;
-    int forcasTimeA[11],forcasTimeB[11];
-    int forca;
-      
-    scanf(" %30[^\n]", nomeDoTimeA);
-    for (int i = 0; i < 11; i++)
-    {
-        scanf(" %30[^;]; %c; %d", nomeJogador,&posicao, &forca);
-        int posic = (posicao == 'L') ? (forcasTimeA[1] == 0) ? 1 : 2 : 
-        (posicao == 'Z') ? (forcasTimeA[3] == 0) ? 3 : 4 : 
-        (posicao == 'V') ? (forcasTimeA[5] == 0) ? 5 : 6 : 
-        (posicao == 'M') ? (forcasTimeA[7] == 0) ? 7 : 8 : 
-        (posicao == 'A') ? (forcasTimeA[9] == 0) ? 9 : 10 : 0;
-
-        if (forcasTimeA[posic] == 0)
-            forcasTimeA[posic] = forca;
-    }
-    
-    scanf(" %30[^\n]", nomeDoTimeB);
+    scanf(" %30[^\n]", nomeTimeA);
     for (int i = 0; i < 11; i++) {
-        scanf(" %30[^;]; %c; %d", nomeJogador, &posicao, &forca);
+        scanf(" %30[^;]; %c; %d", nomeJogador, &posicaoJogador, &forcaJogador);
 
-        int posi = (posicao == 'L') ? (forcasTimeB[1] == 0) ? 1 : 2 : 
-        (posicao == 'Z') ? (forcasTimeB[3] == 0) ? 3 : 4 : 
-        (posicao == 'V') ? (forcasTimeB[5] == 0) ? 5 : 6 : 
-        (posicao == 'M') ? (forcasTimeB[7] == 0) ? 7 : 8 : 
-        (posicao == 'A') ? (forcasTimeB[9] == 0) ? 9 : 10 : 0;
+        int posicao = (posicaoJogador == 'L') ? (forcasTimeA[1] == 0) ? 1 : 2 : 
+        (posicaoJogador == 'Z') ? (forcasTimeA[3] == 0) ? 3 : 4 : 
+        (posicaoJogador == 'V') ? (forcasTimeA[5] == 0) ? 5 : 6 : 
+        (posicaoJogador == 'M') ? (forcasTimeA[7] == 0) ? 7 : 8 : 
+        (posicaoJogador == 'A') ? (forcasTimeA[9] == 0) ? 9 : 10 : 0;
 
-        if (forcasTimeB[posi] == 0)
-            forcasTimeB[posi] = forca;
+        if (forcasTimeA[posicao] == 0)
+            forcasTimeA[posicao] = forcaJogador;
     }
+
+    scanf(" %30[^\n]", nomeTimeB);
+    for (int i = 0; i < 11; i++) {
+        scanf(" %30[^;]; %c; %d", nomeJogador, &posicaoJogador, &forcaJogador);
+
+        int posicao = (posicaoJogador == 'L') ? (forcasTimeB[1] == 0) ? 1 : 2 : 
+        (posicaoJogador == 'Z') ? (forcasTimeB[3] == 0) ? 3 : 4 : 
+        (posicaoJogador == 'V') ? (forcasTimeB[5] == 0) ? 5 : 6 : 
+        (posicaoJogador == 'M') ? (forcasTimeB[7] == 0) ? 7 : 8 : 
+        (posicaoJogador == 'A') ? (forcasTimeB[9] == 0) ? 9 : 10 : 0;
+
+        if (forcasTimeB[posicao] == 0)
+            forcasTimeB[posicao] = forcaJogador;
+    }
+
     float forcaTimeA = calculaForcaPonderada(forcasTimeA);
     float forcaTimeB = calculaForcaPonderada(forcasTimeB);
-    printf("%s: %.2f de forca\n%s: %.2f de forca\n", nomeDoTimeA, forcaTimeA, nomeDoTimeB, forcaTimeB);
 
+    printf("%s: %.2f de forca\n%s: %.2f de forca\n", nomeTimeA, forcaTimeA, nomeTimeB, forcaTimeB);
 
     if (forcaTimeA > forcaTimeB)
-        printf("%s eh mais forte\n", nomeDoTimeA);
+        printf("%s eh mais forte\n", nomeTimeA);
     else if (forcaTimeB > forcaTimeA)
-        printf("%s eh mais forte\n",nomeDoTimeB);
+        printf("%s eh mais forte\n", nomeTimeB);
     else
-        printf("Os times tem a mesma força\n");
-
-    
-
-
+        printf("Os times têm a mesma forca\n");
 
     return 0;
 }
