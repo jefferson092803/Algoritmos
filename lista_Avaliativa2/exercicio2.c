8e3c7158bf9ae0ed39f333af65a9390705f471ef
jefferson092803 <128716417+jefferson092803@users.noreply.github.com>
2023-11-06 17:02:55 -0300

    ..

--- lista_Avaliativa2/exercicio2.c
+++ lista_Avaliativa2/exercicio2.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-float calculaForcaPonderada(int forcas[]) {
-    return (8 * forcas[0] + 10 * (forcas[1] + forcas[2]) + 5 * (forcas[3] + forcas[4]) + 8 * (forcas[5] + forcas[6]) + 11 * (forcas[7] + forcas[8]) + 12 * (forcas[9] + forcas[10])) / 100.0;
-}
-
 int main() {
     int forcasTimeA[11] = {0}, forcasTimeB[11] = {0};
     char nomeTimeA[31], nomeTimeB[31];
     char nomeJogador[31], posicaoJogador;
     int forcaJogador;
-
+    
+    float calculaForcaPonderada(int forcas[]) {
+    return (8 * forcas[0] + 10 * (forcas[1] + forcas[2]) + 5 * (forcas[3] + forcas[4]) + 8 * (forcas[5] + forcas[6]) + 11 * (forcas[7] + forcas[8]) + 12 * (forcas[9] + forcas[10])) / 100.0;
+}
     scanf(" %30[^\n]", nomeTimeA);
     for (int i = 0; i < 11; i++) {
         scanf(" %30[^;]; %c; %d", nomeJogador, &posicaoJogador, &forcaJogador);
